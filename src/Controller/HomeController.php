d841369fa71f32eb7647260bd6aff8cd363c283b
Plp <wengxiaowu@hotmail.com>
2019-05-28 18:36:24 +0200

    2019 05 28 18.36

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response ;
+
+
+class HomeController{
+
+
+    public function index():Response
+    {
+         return new Response(    " salut les gens ");
+    }
+}
