a5c34adc22ace9ee70a9097d916d9369fb8fa68d
Plp <wengxiaowu@hotmail.com>
2019-05-30 01:43:46 +0200

    20190 50 30

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller\Admin;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Property;
+use App\Form\PropertyType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AdminPropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/admin" , name = "admin.property.index")
+     * @return Response
+     */
+    public function index()
+    {
+        $properties = $this->repository->findAll();
+        return $this->render("admin/property/index.html.twig", compact('properties'));
+    }
+
+    /**
+     * @Route("/admin/property/create" , name ="admin.property.new" )
+     */
+    public function new(Request $request)
+    {
+        $property = new Property(); #这里的$property没读取数据库,是空的
+        $form =  $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            # 提交到数据库 需要用到 use Doctrine\Common\Persistence\ObjectManager; public function __construct(PropertyRepository $repository, ObjectManager $em ){$this->em = $em;}
+            $this->em->persist($property); # 比下面的函数edit() 多了这一句 因为$property 是手动加的 
+            $this->em->flush();
+            $this->addFlash('success','Bien creer avec succes');
+
+            return $this->redirectToRoute("admin.property.index");
+        }
+        return $this->render(
+            "admin/property/new.html.twig",
+            [
+                'property' => $property,
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     *  @Route("/admin/property/{id}" , name = "admin.property.edit" , methods="GET|POST"  ) 
+     */
+    public function edit(Property $property, Request $request)
+    {
+        $form =  $this->createForm(PropertyType::class, $property); # load $property  that  id  = { id } 
+
+        # 接受 request 来的数据 包括get post 要先载入use Symfony\Component\HttpFoundation\Request; 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            # 提交到数据库 需要用到 use Doctrine\Common\Persistence\ObjectManager; public function __construct(PropertyRepository $repository, ObjectManager $em ){$this->em = $em;}
+            $this->em->flush();
+            $this->addFlash('success','Bien modifie avec succes');
+            return $this->redirectToRoute("admin.property.index");
+        }
+
+
+
+        return $this->render(
+            "admin/property/edit.html.twig",
+            [
+                'property' => $property,
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     *  @Route("/admin/property/{id}" , name = "admin.property.delete" , methods = "DELETE"  ) 
+     */
+    # 删除数据
+    public function delete(Property $property, Request $request)
+    {
+        if ($this->isCsrfTokenValid("delete" . $property->getId(), $request->get('_token'))) {
+           // dump("suppression");
+             $this->em->remove($property);
+             $this->em->flush();
+             $this->addFlash('success','Bien supprimer avec succes');
+
+           // return new Response("suppression");
+            
+        }
+        return $this->redirectToRoute("admin.property.index");
+    }
+}
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat',ChoiceType::class,['choices'=> $this->getChoices()]) # 相当于 html 的select -- option 
+           // ->add('city',null,['label'=>'Ville']) #把文字city替换为ville
+            ->add('city')
+           ->add('address')
+            ->add('postal_code')
+            ->add('sold')
+            ->add('created_at')
+             #https://symfony.com/doc/current/forms.html#choice-fields
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms' ,
+            # 需要在文件夹translations下建立 forms.fr.yaml 
+            #查找config/packages/translations.yaml 文件中的default_locale: '%locale%' 对应的locale
+            #修改config/packages/services.yaml 是否也是对应的locale 如 parameters: locale: 'en'改为'fr'(forms.fr.yaml)或 'cn'(forms.cn.yaml) 等等 用来自动替换label中的文字
+        ]);
+    }
+    public function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [] ;
+        foreach( $choices as $k=>$v )
+        {
+            $output[$v] = $k ;
+
+        }
+        return $output ; 
+    }
+}
